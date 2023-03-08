8fc33f1b175a52d25d8e4f04f26c640b6ac40192
JohnnyCobra <geanadvance@gmail.com>
2023-03-07 23:44:15 -0300

    ver 1 - add via upload
    calculadora básica

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+
+print('BEM VINDO')
+print('CALCULADORA BÁSICA')
+
+numero1 = float(input('Digite um número:'))
+numero2 = float(input('Digite outro número:'))
+
+print('A soma é: {}'.format(numero1 + numero2))
+print('A multiplicação é: {}'.format(numero1 * numero2))
+print('A divisão é: {}'.format(numero1 / numero2))
+print('A subtração é: {}'.format(numero1 - numero2))
